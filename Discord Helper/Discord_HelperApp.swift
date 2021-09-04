559d0df7f08bca843e59f835b5c83067fd97007f
Ben Cardy <ben.cardy@ocado.com>
2021-09-04 20:55:47 +0100

    Initial Commit

---
+++ Discord Helper/Discord_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Discord_HelperApp.swift
+//  Discord Helper
+//
+//  Created by Ben Cardy on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Discord_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
