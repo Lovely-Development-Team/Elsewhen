7317c825d6ae17f2632f1fc4be516903ac7557c6
David Stephens <git@dwrs.org>
2021-09-06 16:58:36 +0100

    Support dragging out the discord-formatted date

---
+++ Discord Helper/DiscordFormattedDate.swift
@@ -0,0 +1,28 @@
+//
+//  DiscordFormattedDate.swift
+//  DiscordFormattedDate
+//
+//  Created by David on 06/09/2021.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct DiscordFormattedDate: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.system(.body, design: .monospaced))
+            .foregroundColor(.secondary)
+            .onDrag {
+                return NSItemProvider(item: text.data(using: .utf8)! as NSData, typeIdentifier: UTType.utf8PlainText.identifier)
+            }
+    }
+}
+
+struct DiscordFormattedDate_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscordFormattedDate(text: String(describing: Date().timeIntervalSince1970))
+    }
+}
