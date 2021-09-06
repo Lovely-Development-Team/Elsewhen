2ee13e2e4ef0fee42436b7fd7627d877cece05f3
David Stephens <git@dwrs.org>
2021-09-06 23:22:24 +0100

    Add check for previous build

--- automated-checks.sh
+++ automated-checks.sh
@@ -1,6 +1,10 @@
 FILE=build
-if test -f "$FILE"; then
-    # Bump the build version
+FILE_HASH=$(sha3sum build)
+LAST_BUILD_HASH=$(cat last_build 2>/dev/null)
+# Build file must exist and be different from the last we processed
+if test -f "$FILE" -a "$FILE_HASH" != "$LAST_BUILD_HASH"; then
+	rm last_build
+	# Bump the build version
 	agvtool bump
 	# Store the new build number for use in commit
 	NEW_BUILD=$(agvtool what-version -terse)
@@ -15,5 +19,9 @@ if test -f "$FILE"; then
 	git add build
 	git commit -m "Bump build ($NEW_BUILD)"
 	gh pr create --title "Release $NEW_BUILD"
+	# Store hash of this build file
+	rm last_build
+	sha3sum build >> last_build
+	# Return to the main branch
 	git checkout main
 fi
