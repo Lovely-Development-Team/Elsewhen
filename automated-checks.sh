8d59e3062ff24f3367ba60940d4104192f4363a7
Rosemary Orchard <16113535+RosemaryOrchard@users.noreply.github.com>
2021-09-08 00:30:07 +0100

    Add removed file to Git commit

--- automated-checks.sh
+++ automated-checks.sh
@@ -20,6 +20,7 @@ if test -f "$FILE" -a "$FILE_HASH" != "$LAST_BUILD_HASH"; then
 	# Remove file & continue PR
 	rm $FILE
 	git add "$PROJECT_FILE/project.pbxproj"
+	git add "$FILE"
 	git commit -m "Bump build ($NEW_BUILD)"
 	git push -u "release/$NEW_BUILD"
 	gh pr create --title "Release $NEW_BUILD" --body "$(cat $FILE)" -B main
