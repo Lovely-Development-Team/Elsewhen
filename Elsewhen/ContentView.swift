12564d992031820ae4fc7496b1d6e6a72eaf08dc
Ben Cardy <ben.cardy@ocado.com>
2021-09-14 09:57:33 +0100

    Nicer TabBar appearance. Thanks @alexcfaber!

--- Elsewhen/ContentView.swift
+++ Elsewhen/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Ben Cardy on 04/09/2021.
 //
 
+import UIKit
 import SwiftUI
 import MobileCoreServices
 import UniformTypeIdentifiers
@@ -14,12 +15,24 @@ struct ContentView: View {
     
     @State private var selectedTab: Int = 0
     
+    init() {
+        // Disables the shadow pixel above the topbar
+        UITabBar.appearance().clipsToBounds = true
+
+        // Forces an opaque background on the tabbar, regardless of what is below it.
+        UITabBar.appearance().isTranslucent = true
+        UITabBar.appearance().backgroundColor = UIColor.secondarySystemBackground
+        UITabBar.appearance().backgroundImage = UIImage()
+    }
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             TimeCodeGeneratorView()
+                .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom).foregroundColor(Color.secondary.opacity(0.5)), alignment: .bottom)
                 .tabItem { Label("Time Codes", systemImage: "clock") }
                 .tag(0)
             MykeMode()
+                .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom).foregroundColor(Color.secondary.opacity(0.5)), alignment: .bottom)
                 .tabItem { Label("Time List", systemImage: "list.dash") }
                 .tag(1)
         }
