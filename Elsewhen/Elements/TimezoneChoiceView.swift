75321bd7e8caf3b5d5821b89148d1e50cffbfdb1
Ben Cardy <ben.cardy@ocado.com>
2021-09-14 09:46:59 +0100

    Allow users to add favourite time zones to keep at the top of the list

--- Elsewhen/Elements/TimezoneChoiceView.swift
+++ Elsewhen/Elements/TimezoneChoiceView.swift
@@ -17,6 +17,28 @@ struct TimezoneChoiceView: View {
     var selectMultiple: Bool
     @State private var searchTerm: String = ""
     
+    @State private var favouriteTimeZones: Set<TimeZone> = [
+        TimeZone(identifier: "Europe/London")!,
+        TimeZone(identifier: "Africa/Algiers")!,
+    ]
+    
+    private var sortedFilteredTimeZones: [TimeZone] {
+        TimeZone.filtered(by: searchTerm).sorted {
+            let t0IsFavourite = favouriteTimeZones.contains($0)
+            let t1IsFavourite = favouriteTimeZones.contains($1)
+            if t0IsFavourite && t1IsFavourite {
+                return $0.identifier < $1.identifier
+            }
+            if t0IsFavourite {
+                return true
+            }
+            if t1IsFavourite {
+                return false
+            }
+            return $0.identifier < $1.identifier
+        }
+    }
+    
     private func timeZoneIsSelected(_ tz: TimeZone) -> Bool {
         if selectMultiple {
             return selectedTimeZones.contains(tz)
@@ -29,7 +51,7 @@ struct TimezoneChoiceView: View {
         List {
             SearchBar(text: $searchTerm, placeholder: "Search...")
                 .padding(.horizontal, -10)
-            ForEach(TimeZone.filtered(by: searchTerm), id: \.self) { tz in
+            ForEach(sortedFilteredTimeZones, id: \.self) { tz in
                 Button(action: {
                     if self.selectMultiple {
                         if let index = self.selectedTimeZones.firstIndex(of: tz) {
@@ -44,23 +66,18 @@ struct TimezoneChoiceView: View {
                         presentationMode.wrappedValue.dismiss()
                     }
                 }) {
-                    HStack {
-                        Text(tz.friendlyName)
-                        Spacer()
-                        if let abbreviation = tz.fudgedAbbreviation(for: selectedDate) {
-                            Text(abbreviation)
-                                .foregroundColor(.secondary)
-                        }
-                        if timeZoneIsSelected(tz) {
-                            Image(systemName: "checkmark")
-                        }
-                    }
-                    .foregroundColor(timeZoneIsSelected(tz) ? .accentColor : .primary)
+                    TimeZoneChoiceItem(tz: tz, isSelected: timeZoneIsSelected(tz), abbreviation: tz.fudgedAbbreviation(for: selectedDate), favouriteTimeZones: $favouriteTimeZones)
                 }
             }
         }
         .listStyle(PlainListStyle())
         .navigationTitle("Time Zones")
+        .onAppear {
+            favouriteTimeZones = UserDefaults.standard.favouriteTimeZones
+        }
+        .onChange(of: favouriteTimeZones) { newValue in
+            UserDefaults.standard.favouriteTimeZones = newValue
+        }
     }
 }
 
