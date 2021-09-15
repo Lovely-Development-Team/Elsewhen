7e86f5cb41c2a5ca6befb269f6e5dfbdf98502a3
Ben Cardy <ben.cardy@ocado.com>
2021-09-15 21:21:25 +0100

    Abstract KeyboardHelpers out

---
+++ Elsewhen/Helpers/KeyboardHelpers.swift
@@ -0,0 +1,29 @@
+//
+//  KeyboardHelpers.swift
+//  KeyboardHelpers
+//
+//  Created by Ben Cardy on 15/09/2021.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+protocol KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> { get }
+}
+
+extension KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> {
+        Publishers.Merge(
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillShowNotification)
+                .map { _ in true },
+            
+            NotificationCenter.default
+                .publisher(for: UIResponder.keyboardWillHideNotification)
+                .map { _ in false }
+        )
+            .eraseToAnyPublisher()
+    }
+}
