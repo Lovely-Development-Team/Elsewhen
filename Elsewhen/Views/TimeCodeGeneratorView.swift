e56de7342b59f04042ea41d548aaac0d9fe3978a
Ben Cardy <ben.cardy@ocado.com>
2021-09-15 21:21:25 +0100

    Abstract KeyboardHelpers out

--- Elsewhen/Views/TimeCodeGeneratorView.swift
+++ Elsewhen/Views/TimeCodeGeneratorView.swift
@@ -8,27 +8,6 @@
 import SwiftUI
 import MobileCoreServices
 import UniformTypeIdentifiers
-import Combine
-
-/// Publisher to read keyboard changes.
-protocol KeyboardReadable {
-    var keyboardPublisher: AnyPublisher<Bool, Never> { get }
-}
-
-extension KeyboardReadable {
-    var keyboardPublisher: AnyPublisher<Bool, Never> {
-        Publishers.Merge(
-            NotificationCenter.default
-                .publisher(for: UIResponder.keyboardWillShowNotification)
-                .map { _ in true },
-            
-            NotificationCenter.default
-                .publisher(for: UIResponder.keyboardWillHideNotification)
-                .map { _ in false }
-        )
-            .eraseToAnyPublisher()
-    }
-}
 
 struct TimeCodeGeneratorView: View, KeyboardReadable {
     
