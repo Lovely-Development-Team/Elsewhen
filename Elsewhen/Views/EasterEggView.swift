1c9291ba2e21ab10266fdc7662f7963327051a74
Ben Cardy <ben.cardy@ocado.com>
2021-09-15 19:40:21 +0100

    Add Easter Egg view (hey TIldy!)

---
+++ Elsewhen/Views/EasterEggView.swift
@@ -0,0 +1,128 @@
+//
+//  EasterEggView.swift
+//  EasterEggView
+//
+//  Created by Ben Cardy on 15/09/2021.
+//
+
+import SwiftUI
+
+struct EasterEggView: View {
+    
+    @Environment(\.openURL) var openURL
+    
+    @State private var animate = false
+    @State private var animationType: Animation? = .none
+    #if !os(macOS)
+    let bounceHaptics = UIImpactFeedbackGenerator(style: .light)
+    let selectionHaptics = UISelectionFeedbackGenerator()
+    #endif
+    
+    @State private var showFullL2CUName = false
+    private var affirmationToShow: String = "Teamwork makes the dream work!"
+    
+    private let affirmations: [String] = [
+        "Teamwork makes the dream work!",
+        "You can do it!",
+        "Remember to stay hydrated!",
+        "You are so strong.",
+        "Do you need something to eat or drink?",
+        "I am so proud of the progress you've made.",
+    ]
+    
+    init() {
+        affirmationToShow = affirmations.randomElement() ?? "Teamwork makes the dream work!"
+    }
+    
+    var accessibilityLabel: Text {
+        Text("L2CU (\"Lovely to See You\") says \"\(affirmationToShow)\"")
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Hi there!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Button(action: {
+                withAnimation {
+                    #if !os(macOS)
+                    bounceHaptics.impactOccurred()
+                    #endif
+                    self.animate.toggle()
+                    self.animationType = .default
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.animate.toggle()
+                }
+            }) {
+                Image("l2cu")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth:500, maxHeight: 400)
+                    .padding(.bottom, 20)
+                    .padding(.horizontal, 30)
+                    .scaledToFit()
+                    .accessibility(hidden: true)
+                    .offset(x: 0, y: animate ? -5 : 0)
+                    .animation(animate ? .easeInOut(duration: 0.15).repeatForever(autoreverses: true) : animationType)
+            }
+                .buttonStyle(PlainButtonStyle())
+            HStack(spacing: 5) {
+                Button(action: {
+                    withAnimation {
+                        #if !os(macOS)
+                        selectionHaptics.selectionChanged()
+                        #endif
+                        self.showFullL2CUName.toggle()
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            self.showFullL2CUName = false
+                        }
+                    }
+                }) {
+                    Text(showFullL2CUName ? "Lovely to See You" : "L2CU")
+                }
+                .buttonStyle(PlainButtonStyle())
+                Text("says:")
+            }
+            .font(.headline)
+            .padding(.bottom, 5.0)
+            Text("“\(affirmationToShow)”")
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .allowsTightening(true)
+                .frame(maxWidth: .infinity, alignment: .center)
+            Spacer()
+            Section{
+                Text("L2CU drawing by rhl_. \nElsewhen crafted with care by The Lovely Developers. ")
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .allowsTightening(true)
+                    .minimumScaleFactor(0.7)
+                    .foregroundColor(.secondary)
+                Button("tildy.dev") {
+                        openURL(URL(string: "https://tildy.dev")!)
+                }
+                .padding(.top, 5.0)
+                .padding(.bottom, 10)
+                .allowsTightening(true)
+                .minimumScaleFactor(0.7)
+                .font(.body)
+                .foregroundColor(.blue)
+                .buttonStyle(PlainButtonStyle())
+            }
+            .padding(5.0)
+        }
+        .padding(.top, 30)
+        .padding(10)
+        .accessibilityElement(children: .ignore)
+        .accessibility(label: accessibilityLabel)
+    }
+}
+
+struct EasterEggView_Previews: PreviewProvider {
+    static var previews: some View {
+        EasterEggView()
+    }
+}
