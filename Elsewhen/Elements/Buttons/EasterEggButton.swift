b28928bfed5eafb177ba5de721b76178833b8c06
David Stephens <git@dwrs.org>
2021-09-23 17:55:12 +0100

    Extract easter egg button in separate view

---
+++ Elsewhen/Elements/Buttons/EasterEggButton.swift
@@ -0,0 +1,39 @@
+//
+//  EasterEggButton.swift
+//  Elsewhen
+//
+//  Created by David Stephens on 23/09/2021.
+//
+
+import SwiftUI
+
+struct EasterEggButton: View {
+    let onPress: () -> ()
+    
+    var body: some View {
+        Button(action: {
+            onPress()
+        }, label: {
+            HStack {
+                Text("From the Lovely Developers")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Image("l2culogosvg")
+                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.secondary)
+                    .frame(height: 15)
+                    .accessibility(hidden: true)
+                
+            }
+        })
+            .buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct EasterEggButton_Previews: PreviewProvider {
+    static var previews: some View {
+        EasterEggButton { }
+    }
+}
