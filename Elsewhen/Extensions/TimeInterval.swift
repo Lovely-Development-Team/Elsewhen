c31c1bc8d9325583968b75102d245012261bf8e9
David Stephens <git@dwrs.org>
2021-10-09 15:35:48 +0100

    Add `oneHour` constant to TimeInterval

---
+++ Elsewhen/Extensions/TimeInterval.swift
@@ -0,0 +1,12 @@
+//
+//  TimeInterval.swift
+//  TimeInterval
+//
+//  Created by David on 09/10/2021.
+//
+
+import Foundation
+
+extension TimeInterval {
+    static let oneHour: TimeInterval = 60*60
+}
