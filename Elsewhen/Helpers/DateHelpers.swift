c415f5e5f89f50388b53f14cff6028825cc7ce7e
David Stephens <git@dwrs.org>
2021-10-09 15:02:13 +0100

    Move TimeZone extension into a separate file

--- Elsewhen/Helpers/DateHelpers.swift
+++ Elsewhen/Helpers/DateHelpers.swift
@@ -76,69 +76,8 @@ func discordFormat(for date: Date, in timezone: TimeZone, with formatCode: Forma
     return "<t:\(timeIntervalSince1970):\(formatCode.rawValue)>"
 }
 
-extension TimeZone {
     
-    var friendlyName: String {
-        identifier.replacingOccurrences(of: "_", with: " ")
-    }
-    
-    var flag: String {
-        flagForTimeZone(self)
-    }
-    
-    var isMemberOfEuropeanUnion: Bool {
-        europeanUnionTimeZones.contains(self.identifier)
-    }
-    
-    static func filtered(by searchTerm: String) -> [TimeZone] {
-        let st = searchTerm.trimmingCharacters(in: .whitespaces).lowercased().replacingOccurrences(of: " ", with: "_")
-        return TimeZone.knownTimeZoneIdentifiers.compactMap { tz in
-            TimeZone(identifier: tz)
-        }.filter { $0.matches(searchTerm: st) }
-    }
-    
-    func matches(searchTerm: String) -> Bool {
-        let st = searchTerm.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
-        if st == "" {
-            return true
-        }
-        if identifier.lowercased().contains(st) || friendlyName.lowercased().contains(st) {
-            return true
-        }
-        if let abbreviation = abbreviation(), abbreviation.lowercased().contains(st) {
-            return true
-        }
-        if identifier.starts(with: "America/") {
-            if let localizedName = localizedName(for: .generic, locale: Locale(identifier: "en_US")), localizedName.lowercased().contains(st) {
-                return true
-            }
-        }
-        return false
-    }
-    
-    func fudgedAbbreviation(for selectedDate: Date) -> String? {
-        
-        if identifier.starts(with: "America/") {
-            if let localizedName = localizedName(for: .generic
-                                                                                     , locale: Locale(identifier: "en_US")) {
-                return localizedName.replacingOccurrences(of: " Time", with: "")
-            }
-        }
-        
-        guard let abbreviation = abbreviation(for: selectedDate) else { return nil }
-        let isDaylightSavingTime = isDaylightSavingTime(for: selectedDate)
-        if identifier == "Europe/London" && isDaylightSavingTime {
-            return "BST"
-        }
-        
-        if identifier.starts(with: "Europe") {
-            if isDaylightSavingTime && abbreviation == "GMT+2" || !isDaylightSavingTime && abbreviation == "GMT+1" {
-                return "CET"
-            }
-        }
-        
-        return abbreviation
-        
-    }
-    
+func filter(timezones: [TimeZone], by searchTerm: String) -> [TimeZone] {
+    let st = searchTerm.trimmingCharacters(in: .whitespaces).lowercased().replacingOccurrences(of: " ", with: "_")
+    return timezones.filter { $0.matches(searchTerm: st) }
 }
