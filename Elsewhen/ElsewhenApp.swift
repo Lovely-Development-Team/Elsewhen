66ace4ec5f1602e593e3fae6bb68577f39044a1c
Ben Cardy <ben.cardy@ocado.com>
2021-10-12 23:07:47 +0100

    Move Orientation classes out of App.swift

--- Elsewhen/ElsewhenApp.swift
+++ Elsewhen/ElsewhenApp.swift
@@ -6,46 +6,6 @@
 //
 
 import SwiftUI
-import UIKit
-import Combine
-
-
-enum DeviceOrientation: String {
-    case portrait
-    case landscape
-}
-
-
-class OrientationObserver: ObservableObject {
-    
-    static let shared = OrientationObserver()
-    
-    @Published var currentOrientation: DeviceOrientation = .portrait
-    private var disposables: [AnyCancellable] = []
-    
-    init() {
-        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
-        NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification, object: nil)
-            .sink { notification in
-                print("BEN: Orientation change notification received: \(notification)")
-                if let device = notification.object as? UIDevice {
-                    if device.orientation.isPortrait {
-                        self.changeOrientation(to: .portrait)
-                    } else {
-                        self.changeOrientation(to: .landscape)
-                    }
-                }
-            }
-            .store(in: &disposables)
-    }
-    
-    func changeOrientation(to orientation: DeviceOrientation) {
-        print("BEN: Changing orientation to: \(orientation.rawValue)")
-        currentOrientation = orientation
-    }
-    
-}
-
 
 @main
 struct ElsewhenApp: App {
