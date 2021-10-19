871549902666a2ef6468eb3f9cd0d401b32c46cd
David Stephens <git@dwrs.org>
2021-10-19 23:44:27 +0100

    iOS: "Choose time zones…" should be accent colour
    Fixes #162

--- Elsewhen/Views/MykeMode.swift
+++ Elsewhen/Views/MykeMode.swift
@@ -108,7 +108,9 @@ struct MykeMode: View, OrientationObserving {
             #endif
         }) {
             Text("Choose time zones…")
+            #if os(macOS)
                 .foregroundColor(.primary)
+            #endif
         }
         .popover(isPresented: $showTimeZonePopover, arrowEdge: .leading) {
             TimezoneChoiceView(selectedTimeZone: .constant(TimeZone.current), selectedTimeZones: $selectedTimeZones, selectedDate: $selectedDate, selectMultiple: true)
