2cebc02f9c61b25a194b0e8156c1071583727917
Ben Cardy <ben.cardy@ocado.com>
2021-11-07 20:03:21 +0000

    Add gay wiggles, alphabetise the flags

--- Elsewhen/Helpers/AltIcons.swift
+++ Elsewhen/Helpers/AltIcons.swift
@@ -21,16 +21,18 @@ struct AlternativeIcon {
 let alternativeElsewhenIcons: [AlternativeIcon] = [
     AlternativeIcon(fileName: nil, name: "Original"),
     AlternativeIcon(fileName: "sixcolors", name: "Six Colours"),
+    AlternativeIcon(fileName: "wiggles", name: "Wiggles"),
     AlternativeIcon(fileName: "rainbow", name: "Pride Flag"),
     AlternativeIcon(fileName: "agender", name: "Agender Flag"),
-    AlternativeIcon(fileName: "nonbinary", name: "Nonbinary Flag"),
     AlternativeIcon(fileName: "asexual", name: "Asexual Flag"),
-    AlternativeIcon(fileName: "lesbian", name: "Lesbian Flag"),
-    AlternativeIcon(fileName: "pansexual", name: "Pansexual Flag"),
-    AlternativeIcon(fileName: "genderqueer", name: "Genderqueer Flag"),
-    AlternativeIcon(fileName: "genderfluid", name: "Genderfluid Flag"),
-    AlternativeIcon(fileName: "bisexual", name: "Bisexual Flag"),
     AlternativeIcon(fileName: "aromantic", name: "Aromantic Flag"),
+    AlternativeIcon(fileName: "bisexual", name: "Bisexual Flag"),
+    AlternativeIcon(fileName: "gay", name: "Gay Flag"),
+    AlternativeIcon(fileName: "genderfluid", name: "Genderfluid Flag"),
+    AlternativeIcon(fileName: "genderqueer", name: "Genderqueer Flag"),
+    AlternativeIcon(fileName: "lesbian", name: "Lesbian Flag"),
+    AlternativeIcon(fileName: "nonbinary", name: "Nonbinary Flag"),
+    AlternativeIcon(fileName: "pansexual", name: "Pansexual Flag"),
     AlternativeIcon(fileName: "transgender", name: "Transgender Flag")
 ]
 
