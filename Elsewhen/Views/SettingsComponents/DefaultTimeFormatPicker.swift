fa813db2691d2c9bd48ec2363aab57a828c2f505
Ben Cardy <ben.cardy@ocado.com>
2021-12-29 08:03:28 +0000

    Move Pickers to separate views with titles
    Setting a navigation title on the picker behaves oddly: if set on
    anything other than the last element, it overrides the main view title,
    and if set on the last element, it takes a second or so to appear after
    the view shifts to the picker, which is just odd.

---
+++ Elsewhen/Views/SettingsComponents/DefaultTimeFormatPicker.swift
@@ -0,0 +1,46 @@
+//
+//  DefaultTimeFormatPicker.swift
+//  Elsewhen
+//
+//  Created by Ben Cardy on 29/12/2021.
+//
+
+import SwiftUI
+
+struct DefaultTimeFormatPicker: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var defaultTimeFormat: TimeFormat
+    var body: some View {
+        Form {
+            ForEach(TimeFormat.allCases, id: \.self) { format in
+                Button(action: {
+                    defaultTimeFormat = format
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        /// Without this delay, the binding doesn't properly update and the state doesn't change
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }) {
+                    HStack {
+                        Text(format.description)
+                        Spacer()
+                        if defaultTimeFormat == format {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.accentColor)
+                        }
+                    }
+                    .foregroundColor(.primary)
+                }
+            }
+        }
+        .navigationTitle("Default Time Format")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DefaultTimeFormatPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DefaultTimeFormatPicker(defaultTimeFormat: .constant(.systemLocale))
+        }
+    }
+}
---
+++ Elsewhen/Views/SettingsComponents/SeparatorPicker.swift
@@ -0,0 +1,46 @@
+//
+//  SeparatorPicker.swift
+//  Elsewhen
+//
+//  Created by Ben Cardy on 29/12/2021.
+//
+
+import SwiftUI
+
+struct SeparatorPicker: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var mykeModeSeparator: MykeModeSeparator
+    var body: some View {
+        Form {
+            ForEach(MykeModeSeparator.allCases, id: \.self) { sep in
+                Button(action: {
+                    mykeModeSeparator = sep
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        /// Without this delay, the binding doesn't properly update and the state doesn't change
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }) {
+                    HStack {
+                        Text(sep.description)
+                        Spacer()
+                        if mykeModeSeparator == sep {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.accentColor)
+                        }
+                    }
+                    .foregroundColor(.primary)
+                }
+            }
+        }
+        .navigationTitle("Separator")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SeparatorPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            SeparatorPicker(mykeModeSeparator: .constant(.noSeparator))
+        }
+    }
+}
