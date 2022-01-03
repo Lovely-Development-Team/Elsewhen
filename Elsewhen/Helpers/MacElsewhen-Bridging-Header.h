c688eed26f883b48a0338ffb0172a0085b8b8bc4
Ben Cardy <ben.cardy@ocado.com>
2022-01-03 11:02:54 +0000

    Change copyright to use David's build date Obj-C magic

---
+++ Elsewhen/Helpers/MacElsewhen-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef MacElsewhen_Bridging_Header_h
+#define MacElsewhen_Bridging_Header_h
+
+#import "BuildConstants.h"
+
+#endif /* MacElsewhen_Bridging_Header_h */
