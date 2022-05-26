dd11c6c88fa5b9174de3f784e67614aa0c6483df
Ben Cardy <ben.cardy@ocado.com>
2022-05-26 21:59:13 +0100

    Rename new methods to replace legacy ones

--- Elsewhen/Extensions/NSUbiquitousKeyValueStore.swift
+++ Elsewhen/Extensions/NSUbiquitousKeyValueStore.swift
@@ -29,7 +29,7 @@ extension NSUbiquitousKeyValueStore {
         return "mykeModeTimeZoneGroup:\(name)"
     }
     
-    private func storeTimeZones(_ group: NewTimeZoneGroup) {
+    private func storeTimeZones(_ group: TimeZoneGroup) {
         let timeZoneIdentifiers = group.timeZones.map { $0.identifier }
         let key = makeTimeZoneGroupKey(forName: group.name)
         logger.debug("NSUbiquitousKeyValueStore: Storing time zones for group \(group.name): \(key)=\(timeZoneIdentifiers)")
@@ -42,72 +42,29 @@ extension NSUbiquitousKeyValueStore {
         return identifiers.compactMap { TimeZone(identifier: $0) }
     }
     
-    var newTimeZoneGroups: [NewTimeZoneGroup] {
+    var timeZoneGroups: [TimeZoneGroup] {
         get {
-            let results = timeZoneGroupNames.map { NewTimeZoneGroup(name: $0, timeZones: retrieveTimeZones($0)) }
+            let results = timeZoneGroupNames.map { TimeZoneGroup(name: $0, timeZones: retrieveTimeZones($0)) }
             logger.debug("NSUbiquitousKeyValueStore: Fetched time zone groups: \(results)")
             return results
         }
     }
     
-    func newAddTimeZoneGroup(_ group: NewTimeZoneGroup) {
+    func addTimeZoneGroup(_ group: TimeZoneGroup) {
         logger.debug("NSUbiquitousKeyValueStore: Asked to add: \(group)")
         storeTimeZones(group)
         timeZoneGroupNames = timeZoneGroupNames + [group.name]
     }
     
-    func newRemoveTimeZoneGroup(_ group: NewTimeZoneGroup) {
+    func removeTimeZoneGroup(_ group: TimeZoneGroup) {
         logger.debug("NSUbiquitousKeyValueStore: Asked to remove: \(group)")
         timeZoneGroupNames = timeZoneGroupNames.filter { $0 != group.name }
         removeObject(forKey: makeTimeZoneGroupKey(forName: group.name))
     }
     
-    func newUpdateTimeZoneGroup(_ group: NewTimeZoneGroup, with timeZones: [TimeZone]) {
+    func updateTimeZoneGroup(_ group: TimeZoneGroup, with timeZones: [TimeZone]) {
         logger.debug("NSUbiquitousKeyValueStore: Asked to update \(group.name) with \(timeZones)")
-        storeTimeZones(NewTimeZoneGroup(name: group.name, timeZones: timeZones))
-    }
-    
-    var timeZoneGroups: [TimeZoneGroup] {
-        get {
-            guard let rawData = data(forKey: Self.mykeModeTimeZoneGroupsKey) else {
-                return []
-            }
-            let jsonDecoder = JSONDecoder()
-            do {
-                return try jsonDecoder.decode([TimeZoneGroup].self, from: rawData)
-            } catch {
-                print("Could not decode JSON: \(error)")
-                return []
-            }
-        }
-        set {
-            let jsonEncoder = JSONEncoder()
-            do {
-                let data = try jsonEncoder.encode(newValue)
-                set(data, forKey: Self.mykeModeTimeZoneGroupsKey)
-            } catch {
-                print("Could not encode JSON: \(error)")
-            }
-        }
-    }
-    
-    func addTimeZoneGroup(_ group: TimeZoneGroup) {
-        var tzGroups = timeZoneGroups
-        tzGroups.append(group)
-        timeZoneGroups = tzGroups
-    }
-    
-    func removeTimeZoneGroup(id: UUID) {
-        timeZoneGroups = timeZoneGroups.filter { $0.id != id }
-    }
-    
-    func updateTimeZoneGroup(_ tzGroup: TimeZoneGroup, with timeZones: [TimeZone]) {
-        timeZoneGroups = timeZoneGroups.map { tzGroup in
-            if tzGroup.id == tzGroup.id {
-                return TimeZoneGroup(copy: tzGroup, with: timeZones)
-            }
-            return tzGroup
-        }
+        storeTimeZones(TimeZoneGroup(name: group.name, timeZones: timeZones))
     }
     
 }
--- Elsewhen/Helpers/TimeZoneGroup.swift
+++ Elsewhen/Helpers/TimeZoneGroup.swift
@@ -7,38 +7,12 @@
 
 import Foundation
 
-struct TimeZoneGroup: Codable, Equatable {
-    var id = UUID()
-    let name: String
-    let identifiers: [String]
-    
-    static func == (lhs: TimeZoneGroup, rhs: TimeZoneGroup) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-    var timeZones: [TimeZone] {
-        identifiers.compactMap { TimeZone(identifier: $0) }
-    }
-    
-    init(name: String, timeZones: [TimeZone]) {
-        self.name = name
-        self.identifiers = timeZones.map { $0.identifier }
-    }
-    
-    init(copy: TimeZoneGroup, with timeZones: [TimeZone]) {
-        self.id = copy.id
-        self.name = copy.name
-        self.identifiers = timeZones.map { $0.identifier }
-    }
-    
-}
-
-struct NewTimeZoneGroup: Equatable, CustomStringConvertible {
+struct TimeZoneGroup: Equatable, CustomStringConvertible {
     
     let name: String
     let timeZones: [TimeZone]
     
-    static func == (lhs: NewTimeZoneGroup, rhs: NewTimeZoneGroup) -> Bool {
+    static func == (lhs: TimeZoneGroup, rhs: TimeZoneGroup) -> Bool {
         lhs.name == rhs.name
     }
     
