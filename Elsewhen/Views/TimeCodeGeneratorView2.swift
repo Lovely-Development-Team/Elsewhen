910fd5312cbd8750bf0ef8bce6bafe6722408b9c
Ben Cardy <ben.cardy@ocado.com>
2022-06-20 11:06:05 +0100

    Convert to LazyVGrid

--- Elsewhen/Views/TimeCodeGeneratorView2.swift
+++ Elsewhen/Views/TimeCodeGeneratorView2.swift
@@ -9,36 +9,48 @@ import SwiftUI
 
 struct TimeCodeGeneratorView2: View {
     
+    // MARK: Environment
+    @Environment(\.dynamicTypeSize) var dynamicTypesSize
+    
+    // MARK: State
     @Binding var selectedDate: Date
     @State private var selectedTimeZone: TimeZone? = nil
-    @State private var appendRelative: Bool = false
-    
+    @State private var appendRelative: Bool = true
     @State private var showEasterEggSheet: Bool = false
+    
+    let gridBreakPoint: Int = 170
+    
+    var gridItems: [GridItem] {
+        if dynamicTypesSize >= .xxLarge {
+            return [GridItem(.flexible(), alignment: .top)]
+        }
+        return [GridItem(.adaptive(minimum: CGFloat(gridBreakPoint)), alignment: .top)]
+    }
     
     var body: some View {
         VStack(spacing: 0) {
-//            Text("Time Codes")
-//                .font(.largeTitle)
-//                .frame(minWidth: 0, maxWidth: .infinity)
-//                .background(Color(UIColor.systemBackground))
             Toggle("Include Relative Time", isOn: $appendRelative.animation())
                 .tint(.accentColor)
                 .padding()
                 .background(Color(UIColor.systemBackground))
             ScrollView {
-                ForEach(dateFormats, id: \.self) { dateFormat in
-                    FormatChoiceButton(dateFormat: dateFormat, selectedDate: $selectedDate, appendRelative: $appendRelative, timeZone: $selectedTimeZone)
-                        .padding(.horizontal)
+                LazyVGrid(columns: gridItems, spacing: 0) {
+                    ForEach(dateFormats, id: \.self) { dateFormat in
+                        FormatChoiceButton(dateFormat: dateFormat, selectedDate: $selectedDate, appendRelative: $appendRelative, timeZone: $selectedTimeZone)
+                            .padding(.bottom)
+                    }
+                    FormatChoiceButton(dateFormat: relativeDateFormat, selectedDate: $selectedDate, appendRelative: .constant(false), timeZone: $selectedTimeZone)
+                        .padding(.bottom)
                 }
-                FormatChoiceButton(dateFormat: relativeDateFormat, selectedDate: $selectedDate, appendRelative: .constant(false), timeZone: $selectedTimeZone)
-                    .padding(.horizontal)
+                .fixedSize(horizontal: false, vertical: true)
                 NotRepresentativeWarning()
-                    .padding()
+                    .padding([.horizontal, .bottom])
                 EasterEggButton {
                     showEasterEggSheet = true
                 }
                 .padding(.bottom)
             }
+            .padding(.horizontal)
             .frame(minWidth: 0, maxWidth: .infinity)
             .background(Color.systemBackground)
             DateTimeZoneSheet(selectedDate: $selectedDate, selectedTimeZone: $selectedTimeZone, selectedTimeZones: .constant([]), selectedTimeZoneGroup: .constant(nil), multipleTimeZones: false, saveButtonTapped: nil)
